c17e21074f25229346a5e10436547883e0e4a3aa
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-07 16:10:11 -0600

    "fetches api"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+componentDidMount(){
+
+  fetch('http://jservice.io/api/random')
+  .then(res => res.json())
+  .then(json => {
+    this.setState({
+      isLoaded: true,
+      items: json,
+    })
+  });
+}
+
+render() {
+  var{ isLoaded, items } = this.state;
+
+  if(!isLoaded) {
+    return <div> Loading...</div>
+  }
+
+  else {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+       <h1> JEOPARDY </h1>
+     </header>
+       <ul>
+         {items.map(item => (
+            <li key={item.id}>
+             value:${item.value},
+             question: {item.question} | answer: {item.answer}
+
+            </li>
+         ))};
+       </ul>
     </div>
   );
+}
+}
 }
 
 export default App;
