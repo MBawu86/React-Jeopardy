79824450b032c216cd6e87ce0674ad056cfb561e
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-11 19:17:36 -0500

    final changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -92,45 +91,7 @@ fetch('http://jservice.io/api/random')
 
 }
 
-function JCard() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Jeopardy!
-        <Container>
-          <Form>
-            <Row>
-              <Col md>
-                <Card className= "mb-1" style={{ color: '#000' }}>The Answer is </Card>
-                <Card className= "mb-1" style={{ color: '#000' }}>The API for the answer goes here</Card>
-              </Col>
-       
-              <Col md>
-                <Card className= "mb-1" style={{ color: '#000' }}>Who, what, where is ... </Card>
-                <Card className= "mb-1" style={{ color: '#000' }}>The API question goes here</Card>
-              </Col>
-          </Row>
-        </Form>
-        <Button variant='primary' type='onClick'>Reveal the Question</Button>
-        <Card className= "mb-1" style={{ color: '#000' }}>
-          <Card.Img src='https://i.imgur.com/GNv1eXg.jpeg' />
-        
-        </Card>   
-        </Container>        
-      </header>
-    </div>
-  );
-}
 
 
 export default App;
 
-/* { <ul>
-  {items.map(item => (
-    <li key={item.id}>
-      value:${item.value} |
-      Question: {item.question} |
-      Answer: {item.answer} |
-      Question: {item.question}
-    </li>
-</ul> } */
